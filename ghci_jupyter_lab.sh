ffc505973fbaa94d4611ac99d26475a5f3e2eb9a
TBillTech <tom@TomsEdge.local>
2019-02-09 14:11:12 -0700

    Create ACE extension

--- ghci_jupyter_lab.sh
+++ ghci_jupyter_lab.sh
@@ -1,3 +1,4 @@
 cp Dockerfile.ghci Dockerfile
 sudo docker build -t ighci .
-sudo docker run --rm --name ghci_jupyter_lab -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v $JOVYAN_STACK_PATH:/home/jovyan/.stack -v $JUPYTER_WORKSPACE:/home/jovyan/work ighci
+#sudo docker run --rm --name ghci_jupyter_lab -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v $JOVYAN_STACK_PATH:/home/jovyan/.stack -v $JUPYTER_WORKSPACE:/home/jovyan/work ighci
+sudo docker run --rm --name ghci_jupyter_lab -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v $JOVYAN_STACK_PATH:/home/jovyan/.stack -v $JUPYTER_WORKSPACE:/home/jovyan/work --entrypoint=start.sh ighci jupyter lab --watch
