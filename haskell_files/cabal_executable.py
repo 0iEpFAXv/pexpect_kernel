3d45ae4d6d205a9b8935f0edffbd48c765973d24
TBillTech <tom@TomsEdge.local>
2019-05-11 15:05:12 -0600

    Also allow test-suite to find tests

--- haskell_files/cabal_executable.py
+++ haskell_files/cabal_executable.py
@@ -6,7 +6,7 @@ with open(sys.argv[1], 'r') as cabalfile:
 lookfor = sys.argv[2].strip()
 
 for line in cabal_lines:
-    if 'executable' in line:
+    if 'executable' in line or 'test-suite' in line:
         nospaces = line.strip().replace(' ','')
         nows = nospaces.replace('\t','')
         exename = nows[len('executable'):]
